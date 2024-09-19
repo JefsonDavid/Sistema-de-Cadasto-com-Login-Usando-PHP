afaffd286437244463719fc22c990853e4854cb9
Jéfson David🖖😎 <jefsondavid@gmail.com>
2024-09-18 23:36:07 -0300

    Add files via upload

---
+++ models/Usuario.php
@@ -0,0 +1,89 @@
+<?php
+    class Usuario {
+        private $id;
+        private $nome;
+        private $email;
+        private $telefone;
+        private $sexo;
+        private $nascimento;
+        private $cidade;
+        private $estado;
+        private $endereco;
+
+
+        public function getId() {
+            return $this->id;
+        }
+
+        public function setId($i) {
+            $this->id = trim($i);
+        }
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function setNome($no) {
+            $this->nome = ucwords(trim($no));
+        }
+
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function setEmail($em) {
+            $this->email = strtolower(trim($em));
+        }
+
+        public function getTelefone() {
+            return $this->telefone;
+        }
+
+        public function setTelefone($te) {
+            $this->telefone = $te;
+        }
+
+        public function getSexo() {
+            return $this->sexo;
+        }
+
+        public function setSexo($se) {
+            $this->sexo = $se;
+        }
+
+        public function getNascimento() {
+            return $this->nascimento;
+        }
+
+        public function setNascimento($na) {
+            $this->nascimento = $na;
+        }
+
+        public function getCidade() {
+            return $this->cidade;
+        }
+
+        public function setCidade($ci) {
+            $this->cidade = $ci;
+        }
+
+        public function getEstado() {
+            return $this->estado;
+        }
+
+        public function setEstado($es) {
+            $this->estado = $es;
+        }
+
+        public function getEndereco() {
+            return $this->endereco;
+        }
+
+        public function setEndereco($en) {
+            $this->endereco = $en;
+        } 
+    }
+
+    interface UsuarioDao {
+        public function adicionar(Usuario $u);
+    }
---
+++ models/UsuarioDaoMysql.php
@@ -0,0 +1,27 @@
+<?php
+    require_once 'models/Usuario.php';  
+
+    class UsuarioDaoMysql implements UsuarioDao {
+        private $pdo;
+
+        public function __construct(PDO $driver) {
+            $this->pdo = $driver;
+        }
+
+        public function adicionar(Usuario $u) {
+            $sql = $this->pdo->prepare("INSERT INTO formulario (nome, email, telefone, sexo, data_nasc, cidade, estado, endereco) VALUES (:nome, :email, :telefone, :sexo, :nascimento, :cidade, :estado, :endereco)");
+
+            $sql->bindValue(':nome', $u->getNome());
+            $sql->bindValue(':email', $u->getEmail());
+            $sql->bindValue(':telefone', $u->getTelefone());
+            $sql->bindValue(':sexo', $u->getSexo());
+            $sql->bindValue(':nascimento', $u->getNascimento());
+            $sql->bindValue(':cidade', $u->getCidade());
+            $sql->bindValue(':estado', $u->getEstado());
+            $sql->bindValue(':endereco', $u->getEndereco());
+            $sql->execute();
+
+            $u->setId($this->pdo->lastInsertId());
+            return $u;
+        }
+    }
